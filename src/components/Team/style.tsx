67e385523c7120fa8376f2971a10627a35c8857f
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-11-09 15:52:41 -0300

    criaçao do componente time e alguns ajustes na pagina team

---
+++ src/components/Team/style.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ContainerImg = styled.div`
+    height: 90px;
+    width: 90px;
+    cursor: pointer;
+
+    img{
+        width: 100%;
+    }
+`
+
+export{
+    ContainerImg
+}
